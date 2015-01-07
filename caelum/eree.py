954e366ac03b9be46a06e38e38b407f00609d517
Nathan Charles <ncharles@gmail.com>
2015-01-07 16:48:26 -0500

    initial commit

---
+++ caelum/eree.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# Copyright (C) 2013 Nathan Charles
+#
+# This program is free software. See terms in LICENSE file.
+"""Global EERE Weather Data Sources"""
+
+import os
+import csv
+import zipfile
+import tempfile
+try:
+    from urllib.request import urlopen
+except ImportError:
+    from urllib import urlopen
+
+WEATHER_DATA_PATH = os.environ['HOME'] + "/weather_data"
+SRC_PATH = os.path.dirname(os.path.abspath(__file__))
+
+try:
+    os.listdir(WEATHER_DATA_PATH)
+except OSError:
+    try:
+        os.mkdir(WEATHER_DATA_PATH)
+    except IOError:
+        pass
+
+def download(url):
+    """download TMY3 file"""
+    data_handle = urlopen(url)
+    with tempfile.TemporaryFile(suffix='.zip', dir=WEATHER_DATA_PATH) \
+            as local_file:
+        local_file.write(data_handle.read())
+        compressed_file = zipfile.ZipFile(local_file, 'r')
+        compressed_file.extractall(WEATHER_DATA_PATH)
+        local_file.close()
+
+def _eere_url(station_code):
+    """build EERE EPW url for station code"""
+    baseurl = 'http://apps1.eere.energy.gov/buildings/energyplus/weatherdata/'
+    return baseurl + _station_info(station_code)['url']
+
+def _station_info(station_code):
+    """filename based meta data for a station code"""
+    url_file = open(SRC_PATH + '/eree.csv')
+    for line in csv.DictReader(url_file):
+        if line['station_code'] == station_code:
+            return  line
+    raise Exception('Station not found')
+
+DATA_EXTENTIONS = {'SWERA':'epw'}
+
+def _basename(station_code):
+    "region, country, weather_station, station_code, data_format, url"
+    info = _station_info(station_code)
+    basename = '%s_%s.%s_%s.%s' % (info['country'], \
+            info['weather_station'],\
+            info['station_code'], info['data_format'], \
+            DATA_EXTENTIONS[info['data_format']])
+    return basename
+
+class EPWdata(object):
+    """EPW weather generator"""
+    def __init__(self, station_code):
+        #filename = path + usaf + 'TY.csv'
+        filename = WEATHER_DATA_PATH + '/' + _basename(station_code)
+        self.csvfile = None
+        try:
+            self.csvfile = open(filename)
+        except IOError:
+            print("File not found")
+            print("Downloading ...")
+            download(_eere_url(station_code), WEATHER_DATA_PATH, filename)
+            self.csvfile = open(filename)
+        fieldnames = ["Year", "Month", "Day", "Hour", "Minute", "DS", \
+                "Drybulb (C)", "Dewpoint (C)", "Relative Humidity", \
+                "Pressure (Pa)", "ETR (W/m^2)", "ETRN (W/m^2)", "HIR (W/m^2)", \
+                "GHI (W/m^2)", "DNI (W/m^2)", "DHI (W/m^2)", "GHIL (lux)", \
+                "DNIL (lux)", "DFIL (lux)", "Zlum (Cd/m2)", "Wdir (degrees)", \
+                "Wspd (m/s)", "Ts cover", "O sky cover", "CeilHgt (m)", \
+                "Present Weather", "Pw codes", "Pwat (cm)", "AOD (unitless)", \
+                "Snow Depth (cm)", "Days since snowfall"]
+        dummy = ""
+        for _ in range(7):
+            dummy += self.csvfile.readline()
+        self.epw_data = csv.DictReader(self.csvfile, fieldnames=fieldnames)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        """generator handle"""
+        return self.epw_data.next()
+
+    def __del__(self):
+        self.csvfile.close()
+
+if __name__ == '__main__':
+    SCODE = '418830'
+    for trecord in EPWdata(SCODE):
+        print(trecord)
---
+++ setup.py
@@ -0,0 +1,32 @@
+"""install"""
+import os
+from setuptools import setup, find_packages
+import datetime
+
+NOW = datetime.datetime.now()
+MAJOR = 0
+
+def read(fname):
+    """README helper fuction"""
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="caelum",
+    version="%s.%s.%s.%s" % (MAJOR, NOW.month, NOW.day, NOW.hour),
+    author="Nathan Charles",
+    author_email="ncharles@gmail.com",
+    description=("Python library for typical historical weather sources"),
+    license="LGPL",
+    keywords="climate weather data TMY EPW EERE",
+    url="https://github.com/nrcharles/caelum",
+    packages=find_packages(),
+    long_description=read('README.md'),
+    install_requires=['geopy'],
+    package_data={'': ['*.csv']},
+    test_suite='tests.unit',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        ("License :: OSI Approved ::" \
+            " GNU Library or Lesser General Public License (LGPL)"),
+    ],
+)
