360542fed5d2951d1bade5a77fb4f34dfd640a22
Nathan Charles <ncharles@gmail.com>
2015-02-23 10:50:19 -0500

    pep8

--- caelum/tmy3.py
+++ caelum/tmy3.py
@@ -1,7 +1,11 @@
 """TMY3 data set library: thin wrapper around csv files
 
 Examples:
-    >>> sum([i[''] for i in data(USAF)])
+    >>> sum([int(i['GHI (W/m^2)']) for i in data('724666')])/365./1000.
+    4.438213698630137
+
+    >>> round(total('724666', 'DNI (W/m^2)')/365.,2)
+    5.11
 
 """
 import csv
@@ -28,16 +32,31 @@ except OSError:
 
 
 def tmybasename(usaf):
-    """get basename for USAF base"""
-    url_file = open(SRC_PATH + '/tmy3urls.csv')
+    """Basename for USAF base.
+
+    Args:
+        usaf (str): USAF code
+
+    Returns:
+        (str)
+    """
+    url_file = open(SRC_PATH + '/tmy3.csv')
     for line in url_file.readlines():
         if line.find(usaf) is not -1:
             return line.rstrip().partition(',')[0]
 
 
 def download_tmy(usaf):
-    """download TMY3 file"""
+    """Download TMY3 file.
+
+    Args:
+        usaf (str): USAF code
+
+    Returns:
+        (None)
+    """
     url = "http://rredc.nrel.gov/solar/old_data/nsrdb/1991-2005/data/tmy3/"
+    # url = "http://rredc.nrel.gov/solar/old_data/nsrdb/1991-2005/data/tmy3.20080820/"
     import urllib2
     tmybase = tmybasename(usaf)
     tmydata = urllib2.urlopen(url + tmybase)
@@ -47,34 +66,57 @@ def download_tmy(usaf):
 
 
 def strptime(string, timezone=0):
-    """custom strptime - necessary because of 24:00 end of day labeling"""
+    """custom strptime - necessary because of 24:00 end of day labeling."""
     year = int(string[6:10])
     month = int(string[0:2])
     day = int(string[3:5])
     hour = int(string[11:13])
     minute = int(string[14:16])
-    return datetime.datetime(year, month, day) +
-                             datetime.timedelta(hours=hour, minutes=minute) -
-                             datetime.timedelta(hours=timezone)
+    return datetime.datetime(year, month, day) + \
+        datetime.timedelta(hours=hour, minutes=minute) - \
+        datetime.timedelta(hours=timezone)
 
 
 def normalize_date(tmy_date, year):
-    """change TMY3 date to an arbitrary year"""
+    """change TMY3 date to an arbitrary year.
+
+    Args:
+        tmy_date (datetime): date to mangle.
+        year (int): desired year.
+
+    Returns:
+        (None)
+    """
     month = tmy_date.month
     day = tmy_date.day - 1
     hour = tmy_date.hour
     # hack to get around 24:00 notation
     if month is 1 and day is 0 and hour is 0:
         year = year + 1
-    return datetime.datetime(year, month, 1) +
-                             datetime.timedelta(days=day,
-                                                hours=hour, minutes=0)
+    return datetime.datetime(year, month, 1) + \
+        datetime.timedelta(days=day, hours=hour, minutes=0)
 
 
 class data(object):
-    """TMY3 iterator"""
+
+    """TMY3 iterator object.
+
+    Atributes:
+        latiude (float)
+        longitude (float)
+        tz (float)
+    """
+
     def __init__(self, usaf):
-        filename = TMY_PATH + usaf + 'TY.csv'
+        """initialize.
+
+        Args:
+            usaf (str)
+
+        Returns:
+            (object)
+        """
+        filename = TMY_PATH + usaf + 'TYA.csv'
         self.csvfile = None
         try:
             self.csvfile = open(filename)
@@ -88,14 +130,12 @@ class data(object):
         self.latitude = float(header[4])
         self.longitude = float(header[5])
         self.tz = float(header[3])
-        # print header[1]
-        # print self.latitude, self.longitude
 
     def __iter__(self):
         return self
 
     def next(self):
-        """iterator handle"""
+        """iterate."""
         record = self.tmy_data.next()
         _sd = record['Date (MM/DD/YYYY)'] + ' ' + record['Time (HH:MM)']
         record['utc_datetime'] = strptime(_sd, self.tz)
@@ -107,7 +147,7 @@ class data(object):
 
 
 def total(usaf, field='GHI (W/m^2)'):
-    """total annual insolation, defaults to GHI"""
+    """total annual insolation, defaults to GHI."""
     running_total = 0
     usafdata = data(usaf)
     for record in usafdata:
